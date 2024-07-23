af28ed1478f745db3cbf4347f4a894f032073569
AliM-1997 <aliimortada797@gmail.com>
2024-07-24 02:04:27 +0300

    finishing the list recipes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,13 +8,40 @@ const Home = () => {
       const res = await axios.get(
         "http://localhost//recipes-control-backend/recipes/read-recipe.php"
       );
-      setRecipes(res.data);
+      setRecipes(res.data.recipes);
       console.log(res.data);
     };
     recipe_list();
   }, []);
 
-  return <div className=" page"></div>;
+  return (
+    <div className="page">
+      <h1 className="flex center">All Recipes</h1>
+      <div className="full-width flex space-between padding primary-bg">
+        <h4>recipe name</h4>
+        <h4>recipe description</h4>
+        <h4>recipe ingredients</h4>
+      </div>
+      <div className="full-width ">
+        <ul>
+          {recipes.length > 0 ? (
+            recipes.map((recipe) => (
+              <li
+                key={recipe.id}
+                className="full-width flex space-between padding"
+              >
+                <p>{recipe.name}</p>
+                <p>{recipe.description}</p>
+                <p>Ingredients: {recipe.ingredients}</p>
+              </li>
+            ))
+          ) : (
+            <p>No recipes available.</p>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
